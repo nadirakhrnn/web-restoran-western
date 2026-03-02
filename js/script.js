878391d79bfd143fefa6b70a2369020883eeddf2
nadirakhrnn <252907805+nadirakhrnn@users.noreply.github.com>
2026-03-03 00:45:50 +0700

    Delete js directory

--- js/script.js
+++
@@ -1,5 +0,0 @@
-const tombolPesan = document.getElementById('btnPesan');
-
-tombolPesan.addEventListener('click', function() {
-    alert('Selamat! Kamu mendapatkan diskon 25% untuk pembelian Steak hari ini, dengan minimal pembelian 2 porsi. Gunakan kode promo: STEAK25 saat checkout. Jangan lewatkan kesempatan ini!');
-});
