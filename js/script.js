aeefca02999b259ba0e8e259703c362a38caa51a
nadirakhrnn <252907805+nadirakhrnn@users.noreply.github.com>
2026-03-03 00:47:21 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const tombolPesan = document.getElementById('btnPesan');
+
+tombolPesan.addEventListener('click', function() {
+    alert('Selamat! Kamu mendapatkan diskon 25% untuk pembelian Steak hari ini, dengan minimal pembelian 2 porsi. Gunakan kode promo: STEAK25 saat checkout. Jangan lewatkan kesempatan ini!');
+});
